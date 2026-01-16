72eda831acd61889892311a2e99923eba2d39f92
Rajat <light.rajat+gh@gmail.com>
2026-01-16 23:51:05 +0530

    feat: Added refresh and close button on error page

--- src/monitor_control/main.py
+++ src/monitor_control/main.py
@@ -3,7 +3,7 @@ gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk, GLib
 from monitor_control.window import AppWindow
 from monitor_control import util
-from monitor_control.const import UPDATE_DELAY, ACCELERATION
+from monitor_control.const import ACCELERATION
 
 
 def update_feature(feature: str, target_val: dict, current_val: dict, override: dict, feature_velocity: dict) -> None:
@@ -26,37 +26,21 @@ def update_feature(feature: str, target_val: dict, current_val: dict, override: 
 
     return True
 
-def app__open(window: Gtk.ApplicationWindow) -> None:
-    success = util.detect_monitor()
-    if success:
-        target_val: dict = {'brightness': None, 'contrast': None, 'volume': None}
-        current_val: dict = {'brightness': None, 'contrast': None, 'volume': None}
-        override: dict = {'brightness': False, 'contrast': False, 'volume': False}
-        feature_velocity: dict = {'brightness': 0, 'contrast': 0, 'volume': 0}
-
-        for feature in target_val.keys():
-            val = util.get_feature_value(feature)
-            target_val[feature] = current_val[feature] = val
-        
-        # initialize main gui page
-        window.init_main_page(target_val, override)
-
-        # feature update loop
-        for feature in target_val.keys():
-            GLib.timeout_add(UPDATE_DELAY, update_feature, feature, target_val, current_val, override, feature_velocity)
-    else:
-        window.stack.set_visible_child_name("error_page")
-
 def app__activate(app) -> None:
-    window = AppWindow(app)
+    target_val: dict = {'brightness': None, 'contrast': None, 'volume': None}
+    current_val: dict = {'brightness': None, 'contrast': None, 'volume': None}
+    override: dict = {'brightness': False, 'contrast': False, 'volume': False}
+    feature_velocity: dict = {'brightness': 0, 'contrast': 0, 'volume': 0}
+
+    window = AppWindow(app, target_val, current_val, override, feature_velocity, update_feature)
     window.present()
-    GLib.idle_add(app__open, window)
+
+    GLib.idle_add(window.refresh_app)
 
 def main() -> None:
     app = Gtk.Application(application_id="org.example.hello")
     app.connect("activate", app__activate)
 
-    # gui loop
     app.run(None)
 
 if __name__ == "__main__":
--- src/monitor_control/window.py
+++ src/monitor_control/window.py
@@ -1,7 +1,8 @@
 import gi
 gi.require_version("Gtk", "4.0")
-from gi.repository import Gtk
-from monitor_control.const import UI_FILE_PATH
+from gi.repository import Gtk, GLib
+from monitor_control.const import UI_FILE_PATH, UPDATE_DELAY
+from monitor_control import util
 
 @Gtk.Template(filename=UI_FILE_PATH)
 class AppWindow(Gtk.ApplicationWindow):
@@ -17,30 +18,50 @@ class AppWindow(Gtk.ApplicationWindow):
 
     stack = Gtk.Template.Child()
 
-    def __init__(self, app) -> None:
+    def __init__(self, app, current_val: dict, target_val: dict, override: dict, feature_velocity: dict, update_feature) -> None:
         super().__init__(application=app)
-        self.stack.set_visible_child_name("loading_page")
-
-    def init_main_page(self, target_val: dict, override: dict) -> None:
+        self.current_val = current_val
         self.target_val = target_val
         self.override = override
+        self.feature_velocity = feature_velocity
+        self.update_feature = update_feature
 
-        val = target_val['brightness']
+        self.stack.set_visible_child_name("loading_page")
+
+    def init_main_page(self) -> None:
+        val = self.target_val['brightness']
         self.label_brightness.set_text(str(val))
         self.last_brightness = val
         self.scale_brightness.set_value(val)
 
-        val = target_val['contrast']
+        val = self.target_val['contrast']
         self.label_contrast.set_text(str(val))
         self.last_contrast = val
         self.scale_contrast.set_value(val)
 
-        val = target_val['volume']
+        val = self.target_val['volume']
         self.label_volume.set_text(str(val))
         self.last_volume = val
         self.scale_volume.set_value(val)
 
         self.stack.set_visible_child_name("main_page")
+    
+    def refresh_app(self) -> None:
+        self.stack.set_visible_child_name("loading_page")
+        success: bool = util.detect_monitor()
+        if success:
+            for feature in self.target_val.keys():
+                val = util.get_feature_value(feature)
+                self.target_val[feature] = self.current_val[feature] = val
+            
+            self.init_main_page()
+
+            for feature in self.target_val.keys():
+                GLib.timeout_add(UPDATE_DELAY, self.update_feature, feature, self.target_val, self.current_val, self.override, self.feature_velocity)
+            
+            self.stack.set_visible_child_name("main_page")
+        else:
+            self.stack.set_visible_child_name("error_page")
     
     @Gtk.Template.Callback()
     def on_scale__value_changed(self, slider) -> None:
@@ -66,3 +87,11 @@ class AppWindow(Gtk.ApplicationWindow):
             if abs(value - self.last_volume) > 1:
                 self.override['volume'] = True
             self.last_volume = value
+
+    @Gtk.Template.Callback()
+    def on_close_button__clicked(self, button) -> None:
+        self.get_application().quit()
+
+    @Gtk.Template.Callback()
+    def on_refresh_button__clicked(self, button) -> None:
+        self.refresh_app()
